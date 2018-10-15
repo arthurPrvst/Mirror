2e43650378347b729dba649850f8f9863c1662bb
Arthur Provost <arthurprovost@MacBook-Pro-de-Arthur.local>
2018-10-15 20:00:12 +0200

    Merge attack and UI

--- main.py
+++ main.py
@@ -1,32 +1,42 @@
 import sys
- 
+
+from attack import *
 from PySide2.QtUiTools import QUiLoader
-from PySide2.QtWidgets import QApplication, QPushButton, QLineEdit, QWidget
+from PySide2.QtWidgets import QApplication, QPushButton, QLineEdit, QWidget, QTextBrowser
 from PySide2.QtCore import QFile, QObject
  
 class Form(QObject):
  
     def __init__(self, ui_file, parent=None):
         super(Form, self).__init__(parent)
+
+        #Load and close GUI file
         ui_file = QFile(ui_file)
         ui_file.open(QFile.ReadOnly)
- 
         loader = QUiLoader()
         self.window = loader.load(ui_file)
         ui_file.close()
 
-        self.centralWidget = self.window.findChild(QWidget, 'centralWidget')
-        self.centralWidget.setStyleSheet("background-image: ./background.png; background-attachment: fixed")
-        self.line = self.window.findChild(QLineEdit, 'lineEdit')
- 
-        btn = self.window.findChild(QPushButton, 'pushButton')
-        btn.clicked.connect(self.ok_handler)
+        #UI components
+        self.btn_scan_ntw = self.window.findChild(QPushButton, 'scanNetwork')
+        self.btn_scan_ntw.clicked.connect(self.scan_handler)
+        self.output_zone = self.window.findChild(QTextBrowser, 'outputZone')
+        self.targetIP = self.window.findChild(QLineEdit, 'targetIP')
+        self.targetMAC = self.window.findChild(QLineEdit, 'targetMAC')
+        self.routerIP = self.window.findChild(QLineEdit, 'routerIP')
+        self.btn_launch_attack = self.window.findChild(QPushButton, 'launchArpAttack')
+        self.btn_launch_attack.clicked.connect(self.attack_handler)
+
         self.window.show()
  
-    def ok_handler(self):
-        language = 'None' if not self.line.text() else self.line.text()
-        print('Favorite language: {}'.format(language))
- 
+    def scan_handler(self):
+        output = launch_scanning_network()
+        self.output_zone.setText(output)
+
+    def attack_handler(self):
+        target_IP = 'None' if not self.targetIP.text() else self.line.text()
+        print('Favorite language: {}'.format(target_IP))
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     form = Form('GUI.ui')
